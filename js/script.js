e0e35b13e7398974bfbfb77cf587f4bdc62e5a85
DuiSeR <dushkafront@gmail.com>
2023-06-07 15:10:54 +0300

    added something in gernes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
+"use strict";
+
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
+  }
+}
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,11 +18,44 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", "");
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log("done");
+    } else {
+      console.log("error");
+      i--;
+    }
+  }
+}
+rememberMyFilms();
 
-console.log(personalMovieDB);
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Посмотрено довольно мало фильмов");
+  } else if (10 <= personalMovieDB.count < 30) {
+    console.log("Вы класический зритель");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+  } else {
+    console.log("Произошла ошибка");
+  }
+}
+detectPersonalLevel();
+
+function showMyDB() {
+  if (personalMovieDB.privat == false) {
+    console.log(personalMovieDB);
+  }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenges() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+  }
+}
+writeYourGenges();
